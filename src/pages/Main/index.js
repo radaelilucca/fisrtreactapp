d00c8c4de72c381b18c579c62e39a2985cd59fed
radaelilucca <luccaradaeli@gmail.com>
2020-01-05 18:30:09 -0300

    Styling main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import { Title } from './styles';
-// import { Container } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Form, Container, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Hello Reacteiros</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositório" />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
