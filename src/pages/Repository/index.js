33b2012500e22e165dcf14703cede0edee93a9f8
Lucca Radaeli <radaeli@MacBook-Pro-de-Lucca.local>
2020-01-13 22:37:27 -0300

    Repository Issues List

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -5,7 +5,7 @@ import { FaArrowAltCircleLeft } from 'react-icons/fa';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssuesList } from './styles';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -62,6 +62,22 @@ export default class Repository extends Component {
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Owner>
+        <IssuesList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                  <p>Author: {issue.user.login}</p>
+                </strong>
+              </div>
+            </li>
+          ))}
+        </IssuesList>
       </Container>
     );
   }
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -24,7 +24,7 @@ export const Owner = styled.div`
   }
 
   h1 {
-    font-size: 30px;
+    font-size: 35px;
     margin-bottom: 5px;
   }
 
@@ -40,7 +40,65 @@ export const Owner = styled.div`
     text-decoration: none;
     margin-bottom: 15px;
     color: #7159c1;
-    font-size: 16px;
+    font-size: 19px;
     justify-content: center;
+  }
+`;
+
+export const IssuesList = styled.div`
+  padding-top: 22px;
+  margin-top: 22px;
+  border-top: 1px solid #eee;
+
+  flex-direction: column;
+  list-style: none;
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+  }
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 2px solid #eee;
+  }
+
+  div {
+    flex: 1;
+    margin-left: 15px;
+
+    strong {
+      font-size: 16px;
+    }
+
+    a {
+      text-decoration: none;
+      color: #333;
+      &:hover {
+        color: #7159c1;
+      }
+    }
+    span {
+      background: #7159f3;
+      color: #eee;
+      border-radius: 2px;
+      font-size: 12px;
+      font-weight: 600;
+      height: 20px;
+      padding: 3px 4px;
+      margin-left: 10px;
+    }
+
+    p {
+      font-size: 15px;
+      color: #999;
+      margin-top: 5px;
+    }
   }
 `;
